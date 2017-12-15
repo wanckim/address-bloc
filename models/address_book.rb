8ca927ee647d5bce86e5da17d292f8cc1f80d2cd
Wan Cheul Kim <wckim88@gmail.com>
2017-12-14 21:15:31 -0800

    Checkpoint3_6: Functional test and funtional import_from_csv method

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -28,4 +29,14 @@ class AddressBook
     end
     entries.delete_at(index)
   end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+  
 end
