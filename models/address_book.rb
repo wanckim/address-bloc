b8c6a506539ed8cee0a25d72599f335343259f99
Wan Cheul Kim <wckim88@gmail.com>
2017-12-14 18:43:09 -0800

    Assignment3_4 - remove_entry method added

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,15 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each_with_index do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        break
+      end
+      index += 1
+    end
+    entries.delete_at(index)
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,14 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes the matching entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.size).to eq(0)
+    end
+  end
 end
